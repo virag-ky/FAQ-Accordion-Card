94d9e1dbaf2990628df2f69fa46ae5f8a362e812
Virag Kormoczy <vkormoczy@gmail.com>
2022-03-20 23:17:33 +0200

    Add desktop version

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const arrowBtn = [...document.querySelectorAll(".title > img")];
+const arrowBtn = [...document.querySelectorAll('.title > img')];
 
 for (let i = 0; i < arrowBtn.length; i += 1) {
-  arrowBtn[i].addEventListener("click", () => {
-    arrowBtn[i].classList.toggle("rotate");
+  arrowBtn[i].addEventListener('click', () => {
+    arrowBtn[i].classList.toggle('rotate');
     const answer = document.getElementById(`p-${i + 1}`);
-    answer.classList.toggle("show");
+    answer.classList.toggle('show');
     const question = document.getElementById(`question-${i + 1}`);
-    question.classList.toggle("bold");
+    question.classList.toggle('bold');
   });
 }
